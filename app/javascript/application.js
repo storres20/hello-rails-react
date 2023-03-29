9b2ae7d6875058d0a8e031f12fa492953eace69a
Italo Lon kan <sanchotorres777@gmail.com>
2023-03-29 12:53:05 -0500

    Update application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,16 +3,17 @@ import '@hotwired/turbo-rails';
 import './controllers';
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 // Redux
 import { Provider } from 'react-redux';
 import store from './react/redux/configureStore';
 
 import App from './react/components/App';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById('root'),
+  </Provider>
+  ,
 );
