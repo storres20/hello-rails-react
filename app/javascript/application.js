d0d3be7f3b85b55a9a21d778cbad2f7c863e4d1a
Italo Lon kan <sanchotorres777@gmail.com>
2023-03-29 09:45:05 -0500

    Add hello world view with root

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,15 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+// import "@hotwired/turbo-rails"
+// import "./controllers"
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (<h1>Hello World 2023!</h1>);
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root'),
+);
