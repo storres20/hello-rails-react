0a1a1fd067d5c93b5ec98e4f42014f430a54a0b9
Italo Lon kan <sanchotorres777@gmail.com>
2023-03-29 12:25:21 -0500

    Add greetings controllers and update routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root 'root#index'
+  # root 'root#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :greetings, only: [:index]
+    end
+  end
+
+  root 'static#index'
 end
